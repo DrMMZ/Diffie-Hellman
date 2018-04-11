4c174eb6e89250ca4736b2577d959f1683a01780
Ming Ming Zhang <38026940+DrMMZ@users.noreply.github.com>
2018-04-11 12:10:57 -0400

    Update DH.py

--- DH.py
+++ DH.py
@@ -1,22 +1,27 @@
-import getpass # Hide typing
-import re # Regular expression
+import getpass 
+import re
 
-# Find all generators of the multiplicative group Z_{p} of integers mod prime p.
+# I. Preliminary. We define a function as follows.
+
+# 1. The function generators(p) finds all generators of the multiplicative group Z_{p} of integers mod prime p. 
 # Note that the complexity of generators() is exponential.
+# Ex. Let p = 5. Then generators(p) = {2, 3}.
 def generators(p) :
     L = set()
-    for x in range(1,p) : # 1 \leq x \leq p-1
+    for x in range(1,p) : # 1 <= x <= p-1
         #print(x)
         S_x = set()
-        for i in range(1,p) : # 1 \leq i \leq p-1
+        for i in range(1,p) : 
             #print(x,"to",i)
             y = (x**i)%p # y = x^{i} mod p
             #print(y)
-            S_x.add(y) # set() is a set of distinct elements, while list() can contain same elements.
+            S_x.add(y)
         print("<",x,"> =",S_x)
         if len(S_x) == p-1 :
             L.add(x)
     print("\nGenerators of Z mod",p,"are",L)
+    
+# II. The Diffie-Hellman protocol.
 
 print()
 print("------------------------------------------------------------")
@@ -24,7 +29,7 @@ print("     The Diffie-Hellman public key exchange protocol        ")
 print("------------------------------------------------------------")
 print()
 
-# Public: a prime p and generator g s.t. <g> = Z_{p}
+# 1. Public: a prime p and generator g s.t. <g> = Z_{p}
 p = input("Choose a prime: ")
 p = int(p)
 print()
@@ -34,11 +39,11 @@ g = input("Choose a generator: ")
 g = int(g)
 print()
 
-# Alice (or Bob) picks a secret integer a (or b, respectively) in [1,p-1].
+# 2. Alice (or Bob) picks a secret integer a (or b, respectively) in [1,p-1].
 q = p-1
 while True :
     print("Choose an integer in [",1,",",q,"]")
-    a = getpass.getpass() # Hide the typing of the secret integer
+    a = getpass.getpass() 
     if re.findall("[a-zA-Z]",a) or int(a) <= 0 or int(a) > q:
         print("Wrong format!")
     else :
@@ -47,7 +52,7 @@ while True :
 
 print()
 
-# Alice (or Bob) sends g^{a} (or g^{b}, respectively) publicly to Bob (or Alice, respectively).
+# 3. Alice (or Bob) sends g^{a} (or g^{b}, respectively) publicly to Bob (or Alice, respectively).
 s = (g**a)%p # s = g^{a} mod p
 print("Send:",s)
 r = input("Receive: ") # r is the received element from Bob (or Alice)
@@ -55,7 +60,7 @@ r = int(r)
 
 print()
 
-# The secret common key k = g^{ab} = g^{ba} mod p.
+# 4. The secret common key k = g^{ab} = g^{ba} mod p.
 k = (r**a)%p
 print("The secret common key:",k)
 
